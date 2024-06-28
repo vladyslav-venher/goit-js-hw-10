53627f609b991887ec02eb912cb0a4d43b0d29d3
vladyslav-venher <vladyslavvenher@gmail.com>
2024-06-28 11:37:01 +0000

    Deploying to gh-pages from @ vladyslav-venher/goit-js-hw-10@22540436ca189ece48e664adc6ae18d24bf6df9d 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as r}from"./assets/vendor-77e16229.js";const m=document.querySelector(".form");m.addEventListener("submit",a);function a(t){t.preventDefault();const s=parseInt(t.target.delay.value),i=t.target.state.value;new Promise((e,o)=>{setTimeout(()=>{i==="fulfilled"?e(s):o(s)},s)}).then(e=>{r.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})}
+//# sourceMappingURL=commonHelpers2.js.map
