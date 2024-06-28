22540436ca189ece48e664adc6ae18d24bf6df9d
Vladyslav Venher <vladyslavvenher@gmail.com>
2024-06-28 14:36:45 +0300

    do task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,36 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(event) {
+  event.preventDefault();
+  
+  const delay = parseInt(event.target.delay.value);
+  const state = event.target.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+};
